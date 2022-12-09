5f26e28e451ac4fae1aa2964508447b1653ba914
AzkSumWave36 <kazunami36.sum@icloud.com>
2022-12-10 07:51:42 +0900

    一括変換機能の追加 プリセット選択後の扱い方法を変更。 エラー未検証です。

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const readline = require("readline");
 const fs = require("fs");
 const http = require("http");
 const ffmpeg = require("fluent-ffmpeg");
+const path = require("path");
 const questions = text => {
     const interface = readline.createInterface({
         input: process.stdin,
@@ -22,6 +23,9 @@ const time = (sec) => {
     output += (sec).toFixed() + "秒";
     return output;
 };
+const video_extensions = [
+    "mp4", "mov", "mkv", "avi"
+];
 const mb = (byte) => { return (byte / 1024 / 1024).toFixed(1); };
 const start = async () => {
     const req = http.request("http://localhost", {
@@ -41,21 +45,24 @@ const start = async () => {
                 "[2] パスを指定してFFmpegで変換を始める (普通)\n" +
                 "[3] FFmpegのタグを自分で入力し、パスを指定して変換を始める (上級)\n" +
                 "[4] デフォルトのパスを変更する\n" +
-                "[5] プリセットを追加・削除する ※現在は使用出来ない機能です。"
+                "[5] プリセットを追加・削除する ※現在は使用出来ない機能です。\n" +
+                "[6] 変換元を指定して、プリセットを使用し一括変換する。"
             );
             const selectcmd = Number(await questions("実行するプログラムを選択してください。> "));
-            if (!selectcmd || selectcmd > 5) { console.error("入力された値が不明であったため、操作を中断します。"); process.exit() };
+            if (!selectcmd || selectcmd > 6) { console.error("入力された値が不明であったため、操作を中断します。"); process.exit() };
             switch (selectcmd) {
                 case 1: {
                     const filename = await questions("ファイル名を入力してください。> ");
-                    presetselect({ inloca: json.in_location, outloca: json.out_location, name: filename });
+                    const presets = await presetselect(json.presets);
+                    convert({ inloca: json.in_location, outloca: json.out_location, name: filename }, presets);
                     break;
                 }
                 case 2: {
                     const in_location = await questions("変換元の動画パスを入力してください。> ");
                     const out_location = await questions("変換先のパスを入力してください。 > ");
                     const filename = await questions("ファイル名を入力してください。> ");
-                    presetselect({ inloca: in_location, outloca: out_location, name: filename });
+                    const presets = await presetselect(json.presets);
+                    convert({ inloca: in_location, outloca: out_location, name: filename }, presets);
                     break;
                 }
                 case 3: {
@@ -78,7 +85,7 @@ const start = async () => {
                     const in_location = await questions("変換元の動画パスを入力してください。> ");
                     if (in_location) json.in_location = in_location;
                     const out_location = await questions("変換先のパスを入力してください。 > ");
-                    if (out_location) json.out_location = out_location;const req = http.request("http://localhost", {
+                    if (out_location) json.out_location = out_location; const req = http.request("http://localhost", {
                         port: 3000,
                         method: "post",
                         headers: { "Content-Type": "text/plain;charset=utf-8" }
@@ -89,6 +96,28 @@ const start = async () => {
                 }
                 case 5: {
                 }
+                case 6: {
+                    let ext = "";
+                    for (let i = 0; i != video_extensions.length; i++) {
+                        ext += video_extensions[i];
+                        if (video_extensions.length != (i + 1)) ext += ", ";
+                    };
+                    console.info(
+                        "パスはフォルダである必要があり、\n" +
+                        "拡張子は[" + ext + "]の" + video_extensions.length + "つとなっています。"
+                    );
+                    const in_location = await questions("変換元のパスを入力してください。> ");
+                    const {filelist, filenamelist} = await filelistget(in_location);
+                    console.log(filenamelist);
+                    const out_location = await questions("変換先のパスを入力してください。 > ");
+                    const presets = await presetselect(json.presets);
+                    const premission = await questions("準備が整いました。変換を開始しますか？yで続行します。 > ");
+                    if (premission != "y") return;
+                    for (let i = 0; i != filelist; i++) {
+                        console.info(i + "度目の変換をしています...\nファイル名: " + filenamelist[i]);
+                        convert({ inloca: in_location + filelist[i], outloca: out_location, name: filenamelist[i] }, presets);
+                    };
+                }
             };
         });
     });
@@ -96,33 +125,20 @@ const start = async () => {
     req.on("error", err => console.log());
     req.end();
 };
-const presetselect = async datas => {
-    const req = http.request("http://localhost", {
-        port: 3000,
-        method: "post",
-        headers: { "Content-Type": "text/plain;charset=utf-8" }
-    });
-    req.on("response", res => {
-        let data = "";
-        res.on("data", chunk => { data += chunk; });
-        res.on("end", async () => {
-            const json = JSON.parse(data);
-            let list = "";
-            for (let i = 0; i != json.presets.length; i++) {
-                list += "[" + (i + 1) + "] " + json.presets[i].display + "\n";
-            };
-            console.info("コマンド一覧:\n" + list);
-            let preset;
-            const selectopt = Number(await questions("変換するプリセットを選択してください。> "));
-            if (!selectopt || selectopt > (json.presets.length)) { console.error("入力された値が不明であったため、操作を中断します。"); process.exit() };
-            convert(datas, json.presets[selectopt - 1].tags);
-        });
-    });
-    req.write(JSON.stringify(["youtube_downloader"]));
-    req.on("error", err => console.log());
-    req.end();
+const presetselect = presets => {
+    let list = "";
+    for (let i = 0; i != presets.length; i++) {
+        list += "[" + (i + 1) + "] " + presets[i].display + "\n";
+    };
+    console.info("コマンド一覧:\n" + list);
+    const selectopt = Number(questions("変換するプリセットを選択してください。> "));
+    if (!selectopt || selectopt > (presets.length)) {
+        console.error("入力された値が不明であったため、操作を中断します。");
+        process.exit();
+    };
+    return presets[selectopt - 1].tags;
 };
-const convert = async (data, preset) => {
+const convert = (data, preset) => {
     const { inloca, outloca, name } = data;
     const prog = ffmpeg(inloca);
     prog.addOptions(preset);
@@ -138,6 +154,30 @@ const convert = async (data, preset) => {
     })
     prog.on("end", () => {
         console.info("動画の変換が完了しました。");
+    });
+};
+const filelistget = folder_path => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(folder_path, { withFileTypes: true }, (err, dirents) => {
+            if (err) reject(err);
+            const filelist = [];
+            const filenamelist = [];
+            for (let i = 0; i != dirents.length; i++) {
+                const dirent = dirents[i];
+                const fp = path.join(folder_path, dirent.name);
+                if (!dirent.isDirectory()) {
+                    const namedot = dirent.name.split(".");
+                    const extension = namedot[namedot.length - 1];
+                    for (let i = 0; i != video_extensions.length; i++) {
+                        if (video_extensions[i] == extension) {
+                            filelist.push(dirent.name);
+                            filenamelist.push(dirent.name.slice(0, -(extension.length + 1)));
+                        };
+                    };
+                };
+            };
+            resolve({filelist: filelist, filenamelist: filenamelist});
+        });
     });
 };
 start();
